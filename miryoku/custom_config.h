ab1fb0bb0def71f7b02db7365330b12db84e604e
Sam Grimee - LX2SG <sgrimee@users.noreply.github.com>
2025-04-20 20:50:52 +0200

    Fix missing layer btn

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,7 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LCTRL, A),    U_MT(LALT, S),     U_MT(LGUI, D),     U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(RGUI, K),     U_MT(LALT, L),     U_MT(RCTRL, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_FUN, ESC),U_LT(U_NUM, SPACE), U_LT(U_NAV, TAB),  U_LT(U_SYM, BSPC),U_LT(U_MEDIA, RET),  U_LT(, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_NUM, SPACE), U_LT(U_NAV, TAB), U_LT(U_SYM, BSPC), U_LT(U_BUTTON, RET),  U_LT(U_MEDIA, DEL), U_NP,             U_NP
 
 #define MIRYOKU_LAYER_TAP \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -12,8 +12,8 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 
 #define MIRYOKU_LAYER_BUTTON \
 U_RDO,             U_UND,             U_PST,             U_CPY,             U_CUT,             U_CUT,             U_CPY,             U_PST,             U_UND,             U_RDO,             \
-&kp LCTRL,         &kp LALT,          &kp LGUI,          &kp LSHFT,         U_NU,              U_NU,              &kp RSHFT,         &kp RGUI,          &kp LALT,          &kp RCTRL,         \
-U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LCTRL,         &kp LALT,          &kp LGUI,          &kp LSHFT,         U_CPY,              U_NU,              &kp RSHFT,         &kp RGUI,          &kp LALT,          &kp RCTRL,         \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_PST,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 
