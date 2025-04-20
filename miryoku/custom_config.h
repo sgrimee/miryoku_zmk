75fe4ec09f0b7ce5ce0eb54e1bb5d75f24a80ef4
Sam Grimee - LX2SG <sgrimee@users.noreply.github.com>
2025-04-20 21:45:30 +0200

    fix case

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,8 +12,8 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 
 #define MIRYOKU_LAYER_BUTTON \
 U_RDO,             U_UND,             U_PST,             U_CPY,             U_CUT,             U_CUT,             U_CPY,             U_PST,             U_UND,             U_RDO,             \
-&kp LCTRL,         &kp LALT,          &kp LGUI,          &kp LSHFT,         U_CPY,             U_NU,              &kp RSHFT,         &kp RGUI,          &kp LALT,          &kp RCTRL,         \
-U_UND,             U_CUT,             U_CPY,             U_PST,             U_PST,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LCTRL,         &kp LALT,          &kp LGUI,          &kp LSHFT,         U_CPY,             U_CPY,             &kp RSHFT,         &kp RGUI,          &kp LALT,          &kp RCTRL,         \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_PST,             U_PST,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 
@@ -39,7 +39,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,             &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,   \
 &kp LCTRL,         &kp LALT,          &kp LGUI,          &kp LSHFT,         U_NA,             &kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,  \
 U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,             &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,   \
-U_NA,              U_NA,              U_NA,              U_NP,              U_NP,             &kp N0,            &KP DOT,           U_NP,              U_NP,              U_NP
+U_NA,              U_NA,              U_NA,              U_NP,              U_NP,             &kp N0,            &kp DOT,           U_NP,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
